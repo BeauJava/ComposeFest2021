03c5b721a88c6823f2ad64c91485363bc452ed72
pmh <pmh@hwajintni.co.kr>
2021-11-05 15:28:35 +0900

    [w1] 버튼 클릭 시 Column 패딩 증가 + 버튼 텍스트 변경 + 텍스트 추가

--- week 1-Jetpack Compose basics/project_w1/app/src/main/java/com/example/project_w1/MainActivity.kt
+++ week 1-Jetpack Compose basics/project_w1/app/src/main/java/com/example/project_w1/MainActivity.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.project_w1.ui.theme.Project_w1Theme
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,18 +36,33 @@ fun MyApp(ss: List<String> = listOf("android", "compose", "codelab", "gdg")) {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+    val expanded = remember { mutableStateOf(false) }
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = if (expanded.value) 40.dp else 0.dp)
+            ) {
                 Text(text = "Hello~")
                 Text(text = "$name!")
+                if (expanded.value) {
+                    Surface(
+                        color = MaterialTheme.colors.secondary,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(text = name)
+                    }
+                }
             }
             OutlinedButton(
-                onClick = {},
+                onClick = { expanded.value = !expanded.value },
                 modifier = Modifier.align(CenterVertically)
             ) {
-                Text("show you " + name)
+                Text((if (expanded.value) "hide you " else "show you ") + name)
             }
         }
     }
