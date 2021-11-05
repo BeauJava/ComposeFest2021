08084cd801070bd82bd13bc0aa96271b98fd5567
pmh <pmh@hwajintni.co.kr>
2021-11-05 10:20:06 +0900

    [w1] Row 사용, Modifier.weight 사용, Modifier.align 사용, 버튼 composable 사용

--- week 1-Jetpack Compose basics/project_w1/app/src/main/java/com/example/project_w1/MainActivity.kt
+++ week 1-Jetpack Compose basics/project_w1/app/src/main/java/com/example/project_w1/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.project_w1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,9 +37,17 @@ fun MyApp(ss: List<String> = listOf("android", "compose", "codelab", "gdg")) {
 fun Greeting(name: String) {
     Surface(color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
-            Text(text = "Hello~")
-            Text(text = "$name!")
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello~")
+                Text(text = "$name!")
+            }
+            OutlinedButton(
+                onClick = {},
+                modifier = Modifier.align(CenterVertically)
+            ) {
+                Text("show you " + name)
+            }
         }
     }
 }
