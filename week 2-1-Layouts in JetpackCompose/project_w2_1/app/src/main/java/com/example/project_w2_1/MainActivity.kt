7c9526cb9628a2f16ec76926a35924c51d64bb57
pmh <pmh@hwajintni.co.kr>
2021-11-11 17:43:38 +0900

    [w2] Modifier 일반적인 객체임, Modifier의 중첩사용 시 순서 유효성, CompositionLocalProvider 함수 사용해 주입

--- week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
@@ -3,11 +3,23 @@ package com.example.project_w2_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.project_w2_1.ui.theme.Project_w2_1Theme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +27,45 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Project_w2_1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = {})
+            .padding(16.dp)
+    ) {
+        Surface(
+            modifier = modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.3f)
+        ) {
+
+        }
+        Column(
+            modifier = modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sysley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 hours ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Project_w2_1Theme {
-        Greeting("Android")
+fun PhotographerCardPreview() {
+    Project_w2_1Theme() {
+        PhotographerCard()
     }
 }
