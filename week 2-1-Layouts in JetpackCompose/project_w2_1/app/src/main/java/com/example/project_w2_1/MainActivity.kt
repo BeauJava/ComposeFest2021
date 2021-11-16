a52a89ee272977bf64ec7fe617b8eaea23be98b5
pmh <pmh@hwajintni.co.kr>
2021-11-16 13:35:20 +0900

    [w2_1] slots 활용, map & forEach 람다 함수사용, 커스텀 레이아웃 샘플구현

--- week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -69,10 +70,36 @@ fun LayoutCodelab() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
-        SimpleList(modifier)
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("1")
+        Text("2")
+        Text("3")
+        Text("4")
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) {
+        measurables, constraints ->
+        val placeables = measurables.map {
+            measurable ->
+            measurable.measure(constraints)
+        }
+        var yPosition = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach {
+                placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height + 8
+            }
+        }
     }
 }
 
@@ -80,7 +107,7 @@ fun BodyContent(modifier: Modifier = Modifier) {
 @Composable
 fun LayoutCodelabPreview() {
     Project_w2_1Theme {
-        Text("Hi there!", Modifier.padding(top = 32.dp))
+        BodyContent()
     }
 }
 @Preview(showBackground = true)
