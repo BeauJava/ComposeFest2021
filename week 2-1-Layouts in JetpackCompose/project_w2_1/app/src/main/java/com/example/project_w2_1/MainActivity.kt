c96c217571aa9195dbfcd38fc8115d9d5a571839
pmh <pmh@hwajintni.co.kr>
2021-11-16 14:38:44 +0900

    [w2_1] Modifier.horizontalScroll(), rememberScrollState, Card, BorderStroke, RoundedCornerShape, coerceIn, rangeTo, IntArray, forEachIndexed 사용
    staggered 그리드를 커스텀 레이아웃으로 만들어봄

--- week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
@@ -3,15 +3,12 @@ package com.example.project_w2_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AirplaneTicket
@@ -22,6 +19,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.Layout
@@ -68,13 +66,78 @@ fun LayoutCodelab() {
     }
 }
 
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    MyOwnColumn(modifier.padding(8.dp)) {
-        Text("1")
-        Text("2")
-        Text("3")
-        Text("4")
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid(modifier = modifier) {
+            for (t in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = t)
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text)
+        }
+    }
+}
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(content = content, modifier = modifier) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+        val placeables = measurables.mapIndexed { i, measurable ->
+            val placeable = measurable.measure(constraints)
+            val row = i % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+            placeable
+        }
+        val width =
+            rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+                ?: constraints.minWidth
+        val height =
+            rowHeights.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+            placeables.forEachIndexed { i, placeable ->
+                val row = i % rows
+                placeable.placeRelative(x = rowX[row], y = rowY[row])
+                rowX[row] += placeable.width
+            }
+        }
     }
 }
 
@@ -86,16 +149,13 @@ fun MyOwnColumn(
     Layout(
         modifier = modifier,
         content = content
-    ) {
-        measurables, constraints ->
-        val placeables = measurables.map {
-            measurable ->
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
             measurable.measure(constraints)
         }
         var yPosition = 0
         layout(constraints.maxWidth, constraints.maxHeight) {
-            placeables.forEach {
-                placeable ->
+            placeables.forEach { placeable ->
                 placeable.placeRelative(x = 0, y = yPosition)
                 yPosition += placeable.height + 8
             }
@@ -110,17 +170,17 @@ fun LayoutCodelabPreview() {
         BodyContent()
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun LayoutCodelabPreview2() {
     Project_w2_1Theme {
-        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+        Chip(text = "Hi there!")
     }
 }
 
 fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) = this.then(
-    layout {
-        measurable, constraints ->
+    layout { measurable, constraints ->
         val placeable = measurable.measure(constraints)
         check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
         val originalFirstBaseline = placeable[FirstBaseline]
