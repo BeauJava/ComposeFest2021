36b593ce91280f031b403b0230c0f496420315cd
pmh <pmh@hwajintni.co.kr>
2021-11-16 11:21:53 +0900

    [w2_1] LazyColumn composable, items api & it keyword, rememberLazyListState, coil package & rememberImagePainter 사용

--- week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.project_w2_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AirplaneTicket
@@ -23,6 +24,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.project_w2_1.ui.theme.Project_w2_1Theme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +41,7 @@ class MainActivity : ComponentActivity() {
 fun LayoutCodelab() {
     Scaffold(
         topBar = {
-            TopAppBar (
+            TopAppBar(
                 title = {
                     Text(text = "LayoutsCodelab")
                 },
@@ -51,7 +53,11 @@ fun LayoutCodelab() {
             )
         }
     ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
     }
 }
 
@@ -60,13 +66,39 @@ fun BodyContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         Text(text = "Hi there!")
         Text(text = "Thanks for going through the Layouts codelab")
+        SimpleList(modifier)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun LayoutCodelabPreview() {
-    Project_w2_1Theme() {
+    Project_w2_1Theme {
         LayoutCodelab()
+    }
+}
+
+@Composable
+fun SimpleList(modifier: Modifier = Modifier) {
+    val scrollState = rememberLazyListState()
+    LazyColumn(state = scrollState, modifier = modifier) {
+        items(100) {
+            ImageListItem(it)
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item ##$index", style = MaterialTheme.typography.subtitle1)
     }
 }
