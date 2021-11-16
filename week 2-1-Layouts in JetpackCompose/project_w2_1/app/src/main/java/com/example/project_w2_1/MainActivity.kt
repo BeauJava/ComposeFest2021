21811e124feba688265890c274a949a19471120a
pmh <pmh@hwajintni.co.kr>
2021-11-16 11:30:17 +0900

    [w2_1] remeberCoroutineScope suspend function 사용, manual scroll

--- week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/project_w2_1/app/src/main/java/com/example/project_w2_1/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.icons.filled.AirplaneTicket
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.project_w2_1.ui.theme.Project_w2_1Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -81,8 +83,26 @@ fun LayoutCodelabPreview() {
 @Composable
 fun SimpleList(modifier: Modifier = Modifier) {
     val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val listSize = 100
+    Row {
+        Button(onClick = {
+            coroutineScope.launch {
+                scrollState.animateScrollToItem(0)
+            }
+        }) {
+            Text(text = "Scroll to the top")
+        }
+        Button(onClick = {
+            coroutineScope.launch {
+                scrollState.animateScrollToItem(listSize - 1)
+            }
+        }) {
+            Text(text = "Scroll to the end")
+        }
+    }
     LazyColumn(state = scrollState, modifier = modifier) {
-        items(100) {
+        items(listSize) {
             ImageListItem(it)
         }
     }
