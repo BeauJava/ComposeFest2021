9013eec182662535e47f5279918f4cbc33fd9c7d
pmh <pmh@hwajintni.co.kr>
2021-11-25 15:58:31 +0900

    [w2_2] mutablestate의 recompositioning 시점 값을 사용해 특정 composable 제거,추가

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -17,12 +17,7 @@
 package com.codelabs.state.todo
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -48,6 +43,7 @@ fun TodoInputTextField(text: String, onTextChange: (String) -> Unit, modifier: M
 @Composable
 fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     val (text, setText) = remember { mutableStateOf("") }
+    val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default) }
     Column {
         Row(
             Modifier
@@ -63,13 +59,19 @@ fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
             )
             TodoEditButton(
                 onClick = {
-                    onItemComplete(TodoItem(text))
+                    onItemComplete(TodoItem(text, icon))
+                    setIcon(TodoIcon.Default)
                     setText("")
                 },
                 text = "Add",
                 modifier = Modifier.align(Alignment.CenterVertically),
                 enabled = text.isNotBlank()
             )
+        }
+        if (text.isNotBlank()) {
+            AnimatedIconRow(icon, setIcon, Modifier.padding(top = 8.dp))
+        } else {
+            Spacer(Modifier.height(16.dp))
         }
     }
 }
