68ebb6da088122891eaa93161afce097bf910b51
pmh <pmh@hwajintni.co.kr>
2021-11-26 13:35:31 +0900

    [w3_1] custom typography apply to custom theme

--- week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -9,7 +9,8 @@ import androidx.compose.ui.graphics.Color
 fun JetnewsTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         content = content,
-        colors = LightColors
+        colors = LightColors,
+        typography = JetnewsTypography
     )
 }
 
