a2fe4da757734ebf247716253851d0cdc6b03898
pmh <pmh@hwajintni.co.kr>
2021-12-03 14:05:04 +0900

    [w4_1] 탑바 테스트, 에뮬레이터에서 화면 보기위해 Thread.sleep() 사용

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -2,6 +2,8 @@ package com.example.compose.rally
 
 import androidx.compose.material.Text
 import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Assert.fail
 import org.junit.Rule
 import org.junit.Test
@@ -12,9 +14,16 @@ class TopAppBarTest {
 
     @Test
     fun myTest() {
+        val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
-            Text("Hi~")
+            RallyTheme {
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = {},
+                    currentScreen = RallyScreen.Overview
+                )
+            }
         }
-        fail()
+        Thread.sleep(5000)
     }
 }
